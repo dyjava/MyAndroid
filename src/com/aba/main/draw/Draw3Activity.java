94e6ba4e45919ad5a1fde7cd83a4b132b1bd0199
dyong <dyong525@163.com>
2015-02-13 16:40:01 +0800

    update

--- src/com/aba/main/draw/Draw3Activity.java
+++ src/com/aba/main/draw/Draw3Activity.java
@@ -1,14 +1,22 @@
 package com.aba.main.draw;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
 import com.aba.main.BaseActivity;
 import com.aba.main.R;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.Bitmap.CompressFormat;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
@@ -17,6 +25,7 @@ import android.view.View;
 import android.view.View.OnTouchListener;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class Draw3Activity extends BaseActivity{
 	private Bitmap baseBitmap;
@@ -25,7 +34,7 @@ public class Draw3Activity extends BaseActivity{
 	private ImageView iv ;
 	
 	private int paintColor = Color.RED ;
-	private int canvasColor = Color.BLUE ;
+	private int canvasColor = Color.WHITE ;
 	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -99,70 +108,67 @@ public class Draw3Activity extends BaseActivity{
 		getMenuInflater().inflate(R.menu.shudu, menu);
 		SubMenu bgcolor = menu.addSubMenu("背景色") ;
 		SubMenu pencolor = menu.addSubMenu("画笔色") ;
+		menu.add(3, 1, 0, "保存") ;
 		
-		bgcolor.add(0, 1, 0, "红色") ;
-		bgcolor.add(0, 2, 0, "黄色") ;
-		bgcolor.add(0, 3, 0, "蓝色") ;
-		bgcolor.add(0, 4, 0, "绿色") ;
-		bgcolor.add(0, 5, 0, "白色") ;
-		bgcolor.add(0, 6, 0, "黑色") ;
+		/** 
+		 * add()方法的四个参数，依次是：
+         * 1、组别，如果不分组的话就写Menu.NONE,
+         * 2、Id，这个很重要，Android根据这个Id来确定不同的菜单
+         * 3、顺序，那个菜单现在在前面由这个参数的大小决定
+         * 4、文本，菜单的显示文本
+         */
+		bgcolor.add(1, Color.RED, 0, "红色") ;
+		bgcolor.add(1, Color.YELLOW, 0, "黄色") ;
+		bgcolor.add(1, Color.BLUE, 0, "蓝色") ;
+		bgcolor.add(1, Color.GREEN, 0, "绿色") ;
+		bgcolor.add(1, Color.WHITE, 0, "白色") ;
+		bgcolor.add(1, Color.BLACK, 0, "黑色") ;
 		
-		pencolor.add(0, 101, 0, "红色") ;
-		pencolor.add(0, 102, 0, "黄色") ;
-		pencolor.add(0, 103, 0, "蓝色") ;
-		pencolor.add(0, 104, 0, "绿色") ;
-		pencolor.add(0, 105, 0, "白色") ;
-		pencolor.add(0, 106, 0, "黑色") ;
+		pencolor.add(2, Color.RED, 0, "红色") ;
+		pencolor.add(2, Color.YELLOW, 0, "黄色") ;
+		pencolor.add(2, Color.BLUE, 0, "蓝色") ;
+		pencolor.add(2, Color.GREEN, 0, "绿色") ;
+		pencolor.add(2, Color.WHITE, 0, "白色") ;
+		pencolor.add(2, Color.BLACK, 0, "黑色") ;
 		return true;
 	}
 	public boolean onOptionsItemSelected(MenuItem item) {
-		switch(item.getItemId()) {
-			case 1:
-				canvasColor = Color.RED ;
-				break;
-			case 2:
-				canvasColor = Color.YELLOW ;
-				break;
-			case 3:
-				canvasColor = Color.BLUE ;
-				break;
-			case 4:
-				canvasColor = Color.GREEN ;
-				break;
-			case 5:
-				canvasColor = Color.WHITE ;
-				break;
-			case 6:
-				canvasColor = Color.BLACK ;
-				break;
-				
-
-			case 101:
-				paintColor = Color.RED ;
-				break;
-			case 102:
-				paintColor = Color.YELLOW ;
-				break;
-			case 103:
-				paintColor = Color.BLUE ;
-				break;
-			case 104:
-				paintColor = Color.GREEN ;
-				break;
-			case 105:
-				paintColor = Color.WHITE ;
-				break;
-			case 106:
-				paintColor = Color.BLACK ;
-				break;
+		int group = item.getGroupId() ;
+		//保存
+		if(group==3){
+			this.save() ;
+			return true ;
 		}
-		if(item.getItemId()<100){
+		//背景色
+		if(group==1){
+			canvasColor = item.getItemId() ;
 			this.create() ;
-		} else {
+		} else if(group==2){
+			//画笔颜色
+			paintColor = item.getItemId() ;
 			paint.setColor(paintColor);
 		}
 		
 		return true ;
-	}  
+	}
+	
+	public void save() {
+		try {
+			File file = new File("/tmp", System.currentTimeMillis() + ".jpg");
+			OutputStream stream = new FileOutputStream(file);
+			baseBitmap.compress(CompressFormat.JPEG, 100, stream);
+			stream.close();
+			// 模拟一个广播，通知系统sdcard被挂载
+			Intent intent = new Intent();
+			intent.setAction(Intent.ACTION_MEDIA_MOUNTED);
+			intent.setData(Uri.fromFile(Environment.getExternalStorageDirectory()));
+			sendBroadcast(intent);
+			
+			Toast.makeText(this, "保存图片成功", 0).show();
+		} catch (Exception e) {
+			Toast.makeText(this, "保存图片失败", 0).show();
+			e.printStackTrace();
+		}
+	}
 
 }
